4cc1aa286d2a44299b607e44aa623c3ed661c553
Ganglere Research <chris@ganglere.net>
2014-03-24 16:43:02 -0400

    Static pressure update
    Modified calcCpCl.m to accept Pinf and Q as either single values or an
    array.

--- Static Pressure/calcCpCl.m
+++ Static Pressure/calcCpCl.m
@@ -4,11 +4,16 @@ function [ Cp Cl ] = calcCpCl( xc, P, Pinf, Q, close )
 
 if ~exist( 'close', 'var' ), close = false; end
 
+% How many samples are there?
+nSamp = size(P,1);
+
+% Replicate Pinf and/or Q if only one value is provided
+if numel(Pinf)==1, Pinf = repmat(Pinf,nSamp,1); end
+if numel(Q)==1, Q = repmat(Q,nSamp,1); end
+
 % Calculate Cp
-nSets = size(P,1);
-for n = 1:nSets
-% 	Cp(n,:) = ( P(n,:) + Pinf(n) )/Q(n);
-	Cp(n,:) = ( P(n,:) + Pinf )/Q;
+for n = 1:nSamp
+  	Cp(n,:) = ( P(n,:) + Pinf(n) )/Q(n);
 end
 
 % Close the Cp curve
