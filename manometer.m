30b195915958c09e57503154a1ccd8a25e6e2553
Ganglere Research <chris@ganglere.net>
2014-10-22 15:52:34 -0400

    Manometer from workspace
    manometer.m: will now automatically import values from workspace if none
    are provided.

--- manometer.m
+++ manometer.m
@@ -3,6 +3,14 @@ function [ V, Re ] = manometer( Tinf, Po, Pinf, Pamb )
 %must be in SI units.
 
 
+if nargin==0
+	% Import values from workspace if none are provided
+	Tinf	= evalin( 'base', 'Tinf.value' );
+	Po		= evalin( 'base', 'Po.value' );
+	Pinf	= evalin( 'base', 'Pinf.value' );
+	Pamb	= evalin( 'base', 'Pamb.value' );
+end
+
 k	= 1.05;			% Tunnel calibration constant, see Little 2010 p.33
 c	= 0.2032;		% Airfoil chord length [m]
 mu	= 1.8e-5;		% Dynamic viscosity of air [N-s/m2]
